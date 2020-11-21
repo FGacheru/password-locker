675858a965239c9dbe17f5fa9cf860a45e3a1408
b-ndunda <francisgacheru2001@gmail.com>
2020-11-21 23:22:54 +0300

    import unittest

---
+++ users_test.py
@@ -0,0 +1,31 @@
+import unittest 
+from users import User
+
+class TestUser(unittest.TestCase):
+     '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestUser: TestUser class that helps in creating test cases
+    '''
+
+      # Items up here .......
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_users = User("Frank","23456789") # create contact object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_users.username,"Frank")
+        self.assertEqual(self.new_contact.password,"23456789")
+
+
+if __name__ == '__main__':
+    unittest.main()
