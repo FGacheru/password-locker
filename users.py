09468d3424712a64b9553823bcdec61cb7f67202
b-ndunda <francisgacheru2001@gmail.com>
2020-11-21 22:51:12 +0300

    add an empty user_list

---
+++ users.py
@@ -0,0 +1,19 @@
+class User:
+
+     """
+    Class that generates new instances of the user
+    """
+
+    user-list = []
+    
+    def __init__(self,username,password)
+
+     '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            username: New contact username.
+            password : New contact password.
+        '''
+
+
