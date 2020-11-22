0967b958ea722a2862b23a29b5077d9c18c6e4e9
b-ndunda <francisgacheru2001@gmail.com>
2020-11-22 21:31:31 +0300

    import Credentials

--- credentials.py
+++ credentials.py
@@ -1,3 +1,17 @@
 class Credentials:
     
+    """
+    Class that generates new instances of the user
+    """
+
+    credentials_list = []
     
+     # Init method up here
+    def save_credentials(self):
+
+        '''
+        save_credentials method saves credential objects into credentials_list
+        '''
+
+        Credentials.crediantials_list.append(self)
+ 
